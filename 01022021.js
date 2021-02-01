2d5f6ea9ce6817c3c9bb7686e4245833b76a8bcd
Nelo Puchades Bresó <correo@nelo.dev>
2021-02-01 21:46:20 +0100

    Project setup + Challenge of day 01/02/21 finished

---
+++ 01022021.js
@@ -0,0 +1,78 @@
+const snapshot = [
+  { sym: 'GME', cost: 280 },
+  { sym: 'PYPL', cost: 234 },
+  { sym: 'AMZN', cost: 3206 },
+  { sym: 'AMZN', cost: 3213 },
+  { sym: 'GME', cost: 325 },
+  { sym: 'GME', cost: 540 },
+  { sym: 'GME', cost: 300 },
+  { sym: 'AMZN', cost: 3540 },
+  { sym: 'PYPL', cost: 300 },
+  { sym: 'AMZN', cost: 2500 },
+  { sym: 'PYPL', cost: 150 },
+];
+
+/* FIRST APPROACH */
+
+const filterStocks = (oldStocks) => {
+  let newStocks = [];
+  for (let i = 0; i < oldStocks.length; i++) {
+    const newStock = oldStocks[i];
+    const alreadyExists = newStocks.find(
+      (currentStock) => currentStock.sym === newStock.sym
+    );
+    if (!alreadyExists) {
+      newStocks.push(newStock);
+    } else {
+      newStocks = newStocks.map((oldStock) =>
+        oldStock.sym === newStock.sym ? newStock : oldStock
+      );
+    }
+  }
+
+  return newStocks;
+};
+
+console.time('first approach');
+const filteredSnapshot = filterStocks(snapshot);
+console.log('filteredSnapshot', filteredSnapshot);
+console.timeLog('first approach');
+
+/*
+  filteredSnapshot = [
+    {sym: "GME", cost: 300},
+    {sym: "PYPL", cost: 150},
+    {sym: "AMZN", cost: 2500}
+  ]
+  first approach: 0.174072265625 ms
+*/
+
+/* SECOND APPROACH */
+
+const filterStocks2 = (allStocks) => {
+  let newStocks = {};
+  for (let i = 0; i < allStocks.length; i++) {
+    const stock = allStocks[i];
+    newStocks[stock.sym] = stock.cost;
+  }
+
+  const mappedStocks = Object.keys(newStocks).map((sym) => ({
+    sym: sym,
+    cost: newStocks[sym],
+  }));
+  return mappedStocks;
+};
+
+console.time('second approach');
+const filteredSnapshot2 = filterStocks2(snapshot);
+console.log('filteredSnapshot2', filteredSnapshot2);
+console.timeLog('second approach');
+
+/*
+  filteredSnapshot = [
+    {sym: "GME", cost: 300},
+    {sym: "PYPL", cost: 150},
+    {sym: "AMZN", cost: 2500}
+  ]
+  second approach: 0.138916015625 ms
+*/
---
+++ README.md
@@ -0,0 +1,1 @@
+# Cassidoo Challenges - Nelodev Solutions
